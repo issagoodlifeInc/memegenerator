00f42278c003cbb73d8c2c44e0b74aca385221e4
Lesley <leskimuti@gmail.com>
2022-07-15 11:55:05 +0300

    Added use of state to randomize Imgs

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 function App() {
+  // const thingsArray = ["Thing 1", "Thing 2"];
+  // const thing = thingsArray.map(thing => <p key={thing}>{thing}</p>)
+
   return (
     <div className="App">
       <Navbar />
       <Main />
+      {/* {thing} */}
     </div>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,39 @@
+import React from "react";
 import meme from "../images/memeimg.png";
+import memes from "../memesData";
 
-function Main(){
-    return(
-        <main className="container section">
-            <div className="inputs">
-                <input type="text" placeholder="Shut up"/>
-                <input type="text" placeholder="and take my money" />
-            </div>
-            <button className="btn">Get a new meme image🖼</button>
-            <div className="main-img">
-                <img src={meme} alt="Meme" />
-            </div>
-        </main>
-    )
+function Main() {
+  //   console.log(memes.data.memes[Math.floor(Math.random()* 100)]);
+
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getImg() {
+    let meme =
+      memes.data.memes[Math.floor(Math.random() * memes.data.memes.length)];
+    let memeUrl = meme.url;
+
+    setMemeImage(memeUrl);
+
+    console.log(memeUrl);
+  }
+
+  return (
+    <main className="container section">
+      <div className="inputs">
+        <input type="text" placeholder="Shut up" />
+        <input type="text" placeholder="and take my money" />
+      </div>
+      <button onClick={getImg} className="btn">
+        Get a new meme image🖼
+      </button>
+
+      <div className="main-img">
+        <img src={meme} alt="Meme" />
+      </div>
+
+      <img className="meme--img" src={memeImage} alt="" />
+    </main>
+  );
 }
 
 export default Main;
